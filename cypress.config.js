128af50d8c15b7ae7d1dcf7b7693bb1d86f897f6
CEM BESLI <71990140+kranklee@users.noreply.github.com>
2026-07-19 10:03:50 -0400

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    video: true, // <--- BU SATIRI EKLEMEN GEREKİYOR
+    video: true, 
   },
 });
